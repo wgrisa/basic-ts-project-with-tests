2c308716754891c3b170e068be996094d4b46684
william <grisa.william@gmail.com>
2022-03-13 13:48:31 -0300

    Update packages, use yarn and start using jest for testing

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,9 @@
-import { expect } from 'chai'
-
 import { sum } from './'
 
 describe('Summing', () => {
   it('returns the sum of two numbers', () => {
     const result = sum({ numbers: [1, 3] })
 
-    expect(result).to.eql(4)
+    expect(result).toEqual(4)
   })
 })
